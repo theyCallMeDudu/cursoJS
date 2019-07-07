0583f732abce09d36e8baec2bc6cebc59ad46de9
theyCallMeDudu <52638570+theyCallMeDudu@users.noreply.github.com>
2019-07-07 19:54:56 -0300

    Create ex1.js
    Exercícios módulo 1 do curso de JS.

---
+++ ex1.js
@@ -0,0 +1,55 @@
+console.log("Questão 1)");
+console.log("O arquivo 'ex1.js' está funcionando.");
+console.log("--------------------------------------------------");
+
+var nome = "Eduardo";
+var idade = 22;
+
+console.log("Questão 2) Olá, eu me chamo " + nome + ", tenho " + idade
+             + " anos e estou estudando JavaScript.");
+console.log("--------------------------------------------------");
+
+var aluno = "Eduardo Coelho e Silva";
+var matricula = 20181210036;
+var p1 = 7.5;
+var p2 = 10;
+var media = (p1 + p2) / 2;
+
+console.log("Questão 3) O aluno " + aluno + ", matrícula " + matricula + ", obteve a média final: " + media + ".");
+console.log("--------------------------------------------------");
+
+var cel1 = "979749761";
+var cel2 = "79749761";
+
+console.log("Questão 4)");
+console.log("Resultado do teste: " + (cel1.length == 9));
+console.log("--------------------------------------------------");
+
+var base = 32;
+var expoente = 6;
+console.log("Questão 5) " + Math.pow(base, expoente));
+console.log("--------------------------------------------------");
+
+console.log("Questão 6)");
+console.log("typeof, pois é uma palavra reservada.");
+console.log("nome-aluno, pois não é permitido usar hífen.");
+console.log("2a_tentativa, pois começa com número.");
+console.log("nome completo, pois contém espaço.");
+console.log("--------------------------------------------------");
+
+console.log("Questão7)");
+console.log("2525");
+console.log("4");
+console.log(undefined);
+console.log(null);
+console.log(undefined);
+console.log("object");
+console.log("--------------------------------------------------");
+
+console.log("Questão 8)");
+console.log(false);
+console.log(true);
+console.log(true);
+console.log(false);
+console.log("boolean");
+console.log("boolean");
